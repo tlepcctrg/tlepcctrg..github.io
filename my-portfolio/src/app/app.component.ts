6c7c28f0807d91218f691a8b0341ce0a5fa670d6
[Dev] Phutisuk Thammakul <zphutisuk.t@pttdigital.com>
2024-08-30 18:04:18 +0700

    update cotent

--- my-portfolio/src/app/app.component.ts
+++ my-portfolio/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,4 +11,23 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'my-portfolio';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    
+  }
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      // This code will only run on the client side
+      const hamburger = document.querySelector("#hamburger")
+      const menu = document.querySelector("#menu")
+      const moon = document.querySelector("#moon")
+      const body = document.querySelector("body")
+      hamburger?.addEventListener("click", ()=>{
+        menu?.classList.toggle("hidden")
+        //hamburger.classList.toggle("bg-white")
+      })
+    }
+    
+  }
+
 }
